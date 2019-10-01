2027cf0980ef12053c9a5789f909b3a8e2a25b25
AlexWelch181 <55792490+AlexWelch181@users.noreply.github.com>
2019-10-01 16:56:43 +0100

    Reverted from modular as it became too buggy same progress as before, but now bugfixed

--- Database_file.py
+++ Database_file.py
@@ -5,22 +5,21 @@
 
 import mysql.connector
 
-## Connecting to the host database
+# Connecting to the host database
 mydb = mysql.connector.connect(host='db4free.net', user='alex_welch',
                                passwd='A1exandm3',
                                database='projectquiz',
                                buffered=True)
-
 
 import hashlib
 import uuid
 from datetime import date
 from tkinter import messagebox
 
-    
+
 class database:
 
-    ## This initialises the database making sure all the tables are created
+    # This initialises the database making sure all the tables are created
     def __init__(self):
         self.conn = mydb.cursor()
         self.conn.execute('''CREATE TABLE IF NOT EXISTS USERS
@@ -60,42 +59,43 @@ class database:
        FOREIGN KEY (USER) REFERENCES USERS(USER_ID),
        PRIMARY KEY(QUIZ_ASSIGNED, USER));''')
         mydb.commit()
-        self.current_questions=[]
-        self.current_user=''
-        self.quiz_details=[]
-        
-    ## Seraches for a user in the database
+        self.current_questions = []
+        self.current_user = ''
+        self.quiz_details = []
+
+    # Searches for a user in the database
     def search_user(self, target):
         self.conn.execute("SELECT USERNAME FROM USERS WHERE USERNAME='"
                           + target + "'")
         results = self.conn.fetchone()
-        if results == None:
+        if results is None:
             results = []
         if len(results) >= 1:
             return True
 
-    ## Searches for the password in the user table 
+    # Searches for the password in the user table
     def search_password(self, password, user):
-        self.conn.execute("SELECT SALT_VAL FROM USERS WHERE USERNAME='"+ user + "'")
+        self.conn.execute("SELECT SALT_VAL FROM USERS WHERE USERNAME='" + user + "'")
         results = self.conn.fetchone()
         salt = results[0]
         new_pass = password + salt
         self.close_data()
         self.open_data()
-        self.conn.execute("SELECT ENCRYPT_PASS FROM USERS WHERE USERNAME='"+ user + "'")
+        self.conn.execute("SELECT ENCRYPT_PASS FROM USERS WHERE USERNAME='" + user + "'")
         results = self.conn.fetchone()
         hash_pass = hashlib.md5(new_pass.encode())
         coded_pass = hash_pass.hexdigest()
         true_pass = results[0]
         if coded_pass == true_pass:
-            self.current_user=user
+            self.current_user = user
+            print(self.current_user)
             return True
         else:
             return False
 
-    ## Checks if the user is an admin
+    # Checks if the user is an admin
     def admin(self, user):
-        self.conn.execute("SELECT ADMIN FROM USERS WHERE USERNAME='"+ user + "'")
+        self.conn.execute("SELECT ADMIN FROM USERS WHERE USERNAME='" + user + "'")
         results = self.conn.fetchall()
         for row in results:
             admin = row[0]
@@ -104,14 +104,14 @@ class database:
         else:
             return False
 
-    ## This method makes sure the new account's password is encrypted and has a salt to make it even harder to crack
+    # This method makes sure the new account's password is encrypted and has a salt to make it even harder to crack
     def create_account(
-        self,
-        user,
-        password,
-        conf_password,
-        admin,
-        ):
+            self,
+            user,
+            password,
+            conf_password,
+            admin,
+    ):
 
         if admin == 'True':
             admin = 1
@@ -133,15 +133,15 @@ class database:
             messagebox.showerror('Error',
                                  'That username is already taken')
 
-    ## This adds a question to the database 
+    # This adds a question to the database
     def add_question(self, details):
         timer = int(details[5].get())
-        ## checking that the timer is an integer and not any other input 
-        if type(timer)==int:
+        # checking that the timer is an integer and not any other input
+        if type(timer) == int:
             sql = '''INSERT INTO QUESTIONS(QUESTION_TEXT,TIMER) VALUES(%s,%s)'''
             q_text = details[0].get()
             self.conn.execute(sql, [q_text, timer])
-            self.conn.execute("SELECT QUESTION_ID FROM QUESTIONS WHERE QUESTION_TEXT='"+ q_text + "'")
+            self.conn.execute("SELECT QUESTION_ID FROM QUESTIONS WHERE QUESTION_TEXT='" + q_text + "'")
             ID = self.conn.fetchone()[0]
             self.current_questions.append(ID)
             for answer in range(1, 5):
@@ -149,61 +149,61 @@ class database:
                     correct = 1
                 else:
                     correct = 0
-                sql ='''INSERT INTO ANSWERS(ANSWER_TEXT,CORRECT,PARENT_QUESTION) VALUES(%s,%s,%s)'''
+                sql = '''INSERT INTO ANSWERS(ANSWER_TEXT,CORRECT,PARENT_QUESTION) VALUES(%s,%s,%s)'''
                 vals = [str(details[answer].get()), correct, ID]
                 self.conn.execute(sql, vals)
         else:
-            messagebox.showerror('Error','Timer should be an integer')
+            messagebox.showerror('Error', 'Timer should be an integer')
 
-    ## This method assigned the quiz just made by a teacher to
-    ## all of the students in their classs through the link table, quiz questions
-    def question_assigned_to_test(self,topic):
-        if len(self.current_questions)==0:
-            messagebox.showerror('Error','No questions created')
+    # This method assigned the quiz just made by a teacher to
+    # all of the students in their class through the link table, quiz questions
+    def question_assigned_to_test(self, topic):
+        if len(self.current_questions) == 0:
+            messagebox.showerror('Error', 'No questions created')
             pass
-        elif topic=="":
-            messagebox.showerror('Error','No topic given')
+        elif topic == "":
+            messagebox.showerror('Error', 'No topic given')
             pass
         else:
-            sql='''INSERT INTO QUIZ(TOPIC,DATE_SET) VALUES(%s,%s)'''
-            vals=[topic,date.today()]
-            self.conn.execute(sql,vals)
-            quiz_id=self.conn.lastrowid
-            sql='''INSERT INTO QUIZ_QUESTIONS(QUIZ_ID,QUESTION_ID,QUESTION_NUMBER) VALUES(%s,%s,%s)'''
+            sql = '''INSERT INTO QUIZ(TOPIC,DATE_SET) VALUES(%s,%s)'''
+            vals = [topic, date.today()]
+            self.conn.execute(sql, vals)
+            quiz_id = self.conn.lastrowid
+            sql = '''INSERT INTO QUIZ_QUESTIONS(QUIZ_ID,QUESTION_ID,QUESTION_NUMBER) VALUES(%s,%s,%s)'''
             for q in range(len(self.current_questions)):
-                vals=[quiz_id,self.current_questions[q],q]
-                self.conn.execute(sql,vals)
-            sql='''INSERT INTO ASSIGNED_QUIZ(QUIZ_ASSIGNED,USER,COMPLETE,SCORE,DATE_COMPLETED,LATE) VALUES(%s,%s,%s,%s,%s,%s)'''
+                vals = [quiz_id, self.current_questions[q], q]
+                self.conn.execute(sql, vals)
+            sql = '''INSERT INTO ASSIGNED_QUIZ(QUIZ_ASSIGNED,USER,COMPLETE,SCORE,DATE_COMPLETED,LATE) VALUES(%s,%s,%s,%s,%s,%s)'''
             self.conn.execute("SELECT USER_ID FROM USERS")
-            num_users=self.conn.fetchall()
+            num_users = self.conn.fetchall()
             for user in range(len(num_users)):
-                vals=[quiz_id,(num_users[user])[0],0,0,"N/A",0]
-                self.conn.execute(sql,vals)
-            self.current_questions=[]
+                vals = [quiz_id, (num_users[user])[0], 0, 0, "N/A", 0]
+                self.conn.execute(sql, vals)
+            self.current_questions = []
 
-    ## This collects the quizzes that have been assigned to the user and adds them to a list            
+    # This collects the quizzes that have been assigned to the user and adds them to a list
     def retrieve_quizes(self):
-        self.conn.execute("SELECT USER_ID FROM USERS WHERE USERNAME='"+ self.current_user +"'")
-        userID=str(self.conn.fetchone()[0])
-        self.conn.execute("SELECT QUIZ_ASSIGNED FROM ASSIGNED_QUIZ WHERE USER='"+ userID  +"'")
-        quizzes=self.conn.fetchall()
+        print("SELECT USER_ID FROM USERS WHERE USERNAME='" + self.current_user + "'")
+        self.conn.execute("SELECT USER_ID FROM USERS WHERE USERNAME='" + self.current_user + "'")
+        userID = str(self.conn.fetchone())
+        print(userID)
+        self.conn.execute("SELECT QUIZ_ASSIGNED FROM ASSIGNED_QUIZ WHERE USER='" + userID + "'")
+        quizzes = self.conn.fetchall()
         for quiz in range(len(quizzes)):
-            self.conn.execute("SELECT * FROM QUIZ WHERE QUIZ_ID='"+ str(quizzes[quiz][0]) +"'")
-            quiz_data=self.conn.fetchall()
+            self.conn.execute("SELECT * FROM QUIZ WHERE QUIZ_ID='" + str(quizzes[quiz][0]) + "'")
+            quiz_data = self.conn.fetchall()
             for data in range(len(quiz_data[0])):
-                complete_quiz_data=""
-                complete_quiz_data=complete_quiz_data+str(quiz_data[0][data])
+                complete_quiz_data = ""
+                complete_quiz_data = complete_quiz_data + str(quiz_data[0][data])
                 print(complete_quiz_data)
-                if data%3==0 and data!=0:
+                if data % 3 == 0 and data != 0:
                     self.quiz_details.append(complete_quiz_data)
                 print(self.quiz_details)
-                
-            
-    ## Establish a connection to the database
+
+    # Establish a connection to the database
     def open_data(self):
         self.conn = mydb.cursor()
 
-    ## Save all the changes made to the database
+    # Save all the changes made to the database
     def close_data(self):
         mydb.commit()
-
